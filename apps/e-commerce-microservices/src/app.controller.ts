36b16f88dfda8cddf28e582d37146f300f9f9d69
Sameh Dheir <dheir2000@gmail.com>
2026-03-05 13:13:39 +0200

    switch to UUID for User ID and implement RegisterDto with class-validator

--- apps/e-commerce-microservices/src/app.controller.ts
+++ apps/e-commerce-microservices/src/app.controller.ts
@@ -1,32 +1,48 @@
-import { Controller, Post, Body, Inject, Res } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Inject,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
-import { Response } from 'express';
+import type { Response } from 'express';
+import { RegisterDto } from 'apps/auth/src/dto/RegisterDto';
 
 @Controller('auth')
 export class AppController {
-  constructor(@Inject('AUTH_SERVICE') private client: ClientProxy) {}
+  constructor(@Inject('AUTH_SERVICE') private readonly client: ClientProxy) {}
 
   @Post('register')
-  register(@Body() body: any) {
-    return this.client.send({ cmd: 'register' }, body);
+  async register(@Body() registerDto: RegisterDto) {
+    return await lastValueFrom(
+      this.client.send({ cmd: 'register' }, registerDto),
+    );
   }
 
   @Post('login')
-  async login(@Body() body: any, @Res({ passthrough: true }) response: any) {
+  async login(
+    @Body() body: any,
+    @Res({ passthrough: true }) response: Response,
+  ) {
     const result = await lastValueFrom(
       this.client.send({ cmd: 'login' }, body),
     );
 
-    if (result.access_token) {
+    if (result && result.access_token) {
       response.cookie('access_token', result.access_token, {
         httpOnly: true,
         secure: false,
         sameSite: 'lax',
-        maxAge: 3600000, // 1h
+        maxAge: 3600000,
       });
-      return { message: 'Success' };
+
+      delete result.access_token;
+      return { message: 'Logged in successfully', user: result.user };
     }
+
     return result;
   }
 }
